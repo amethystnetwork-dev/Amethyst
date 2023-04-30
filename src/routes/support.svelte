31b3cb9070be35bcc0f3a1e25939694733cd2cbe
Procile <114362384+Procile@users.noreply.github.com>
2023-04-29 21:42:32 -0500

    Update support.svelte

--- src/routes/support.svelte
+++ src/routes/support.svelte
@@ -1,1 +1,14 @@
-w
+<script>
+  import Head from "../components/head.svelte";
+</script>
+<Head localTitle="Amethyst | Credits" />
+
+<div class="alignment-container-1">
+    <h1 class="head">Credits</h1>
+  <div class="credits-container">
+    <p><a href="https://github.com/oHeckGage" target="_blank" rel="noreferrer">Gage</a> - Made this project possible</p>
+    <p>IDontCodee - Amethyst Network</p>
+    <p><a href="https://github.com/Vortexzzzz" target="_blank" rel="noreferrer">Vortex</a> - Amazing CSS styling</p>
+    <p><a href="https://github.com/bobsterlarry">bobsterlarry</a> - Helper and Amethyst Network</p>
+  </div>
+</div>
