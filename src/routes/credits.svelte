94713dd8dc831c4c7084b901d71e865a18dae746
Procile <114362384+Procile@users.noreply.github.com>
2023-05-01 12:31:38 -0500

    Update credits.svelte

--- src/routes/credits.svelte
+++ src/routes/credits.svelte
@@ -6,11 +6,11 @@
 <div class="alignment-container-1">
     <h1 class="head">Credits</h1>
   <div class="credits-container">
-    <p><a href="https://github.com/oHeckGage" target="_blank" rel="noreferrer">Gage</a> - Made this project possible</p>
-    <p>IDontCodee - Amethyst Network</p>
-    <p><a href="https://github.com/mdoryammilwalrus" target="_blank" rel="noreferrer">mdoryammilwalrus</a> - Most of the code</p>
-    <p><a href="https://github.com/Vortexzzzz" target="_blank" rel="noreferrer">Vortex</a> - Amazing CSS styling</p>
-    <p><a href="https://github.com/bobsterlarry">bobsterlarry</a> - Helper and Amethyst Network</p>
+    <h1><a href="https://github.com/oHeckGage" target="_blank" rel="noreferrer">Gage</a> - Made this project possible</h1>
+    <h1>IDontCodee - Amethyst Network</h1>
+    <h1><a href="https://github.com/mdoryammilwalrus" target="_blank" rel="noreferrer">mdoryammilwalrus</a> - Most of the code</h1>
+    <h1><a href="https://github.com/Vortexzzzz" target="_blank" rel="noreferrer">Vortex</a> - Amazing CSS styling</h1>
+    <h1><a href="https://github.com/bobsterlarry">bobsterlarry</a> - Helper and Amethyst Network</h1>
   </div>
 </div>
 
@@ -18,5 +18,7 @@
   .credits-container {
     font-family: Arial, Helvetica, sans-serif;
     text-align: center;
-  }
+    color: var(--header);
+    font-size: 20px;
+  }  
 </style>
