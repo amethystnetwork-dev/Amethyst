5364b0cce7eefe4e09be919f95514ef212cfcdeb
bobsterlarry <120289364+bobsterlarry@users.noreply.github.com>
2023-05-01 18:47:53 -0700

    Update credits.svelte

--- src/routes/credits.svelte
+++ src/routes/credits.svelte
@@ -8,9 +8,8 @@
   <div class="credits-container">
     <h1><a href="https://github.com/oHeckGage" target="_blank" rel="noreferrer">Gage</a> - Made this project possible</h1>
     <h1>IDontCodee - Amethyst Network</h1>
-    <h1><a href="https://github.com/mdoryammilwalrus" target="_blank" rel="noreferrer">mdoryammilwalrus</a> - Most of the code</h1>
     <h1><a href="https://github.com/Vortexzzzz" target="_blank" rel="noreferrer">Vortex</a> - Amazing CSS styling</h1>
-    <h1><a href="https://github.com/bobsterlarry">bobsterlarry</a> - Helper and Amethyst Network</h1>
+    <h1><a href="https://github.com/bobsterlarry">bobsterlarry</a> - Helper and Owner of Amethyst Network</h1>
   </div>
 </div>
 
