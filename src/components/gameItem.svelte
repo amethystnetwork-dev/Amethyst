236d208fa9a852e1ab699b94d22f7df6647e658e
Procile <114362384+Procile@users.noreply.github.com>
2023-05-01 22:37:09 -0500

    Update gameItem.svelte

--- src/components/gameItem.svelte
+++ src/components/gameItem.svelte
@@ -5,7 +5,8 @@
     export let click;
 </script>
 
-<div class="gs-item">
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="box" on:click={(event) => click(location, event)}>
     <img class="gs-img" src={image} alt={title} />
     <h7 class="gs-box-header">{title}</h7>
 </div>
@@ -38,18 +39,14 @@
         font-family: "Montserrat", sans-serif;
         color: white;
         text-align: center;
+        margin-left: 140px;
         position: absolute;
         font-size: 30px;
-        margin-left: 100px;
-        white-space: nowrap;
     }
     .gs-box-header:hover {
         transition: 1s;
         transform: scale(1.1);
         z-index: 2;
-    }
-    .gs-item {
-        display: flex;
     }
     .b2 {
         width: 100%;
@@ -58,6 +55,7 @@
         justify-content: center;
         align-items: center;
         margin-top: 10px;
+        /* margin-bottom: 30px; */
     }
     .gs-box-button:hover {
         color: black;
