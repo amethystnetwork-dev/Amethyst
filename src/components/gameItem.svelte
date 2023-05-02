a12e60542a4c75f370ce713b17145e7b2bd2b58b
Procile <114362384+Procile@users.noreply.github.com>
2023-05-01 20:39:50 -0500

    Update gameItem.svelte

--- src/components/gameItem.svelte
+++ src/components/gameItem.svelte
@@ -39,7 +39,7 @@
         font-family: "Montserrat", sans-serif;
         color: white;
         text-align: center;
-        margin-top: 50px;
+        margin-top: 20px;
         margin-left: 100px;
         position: absolute;
         font-size: 30px;
